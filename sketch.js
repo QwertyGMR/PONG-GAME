7e1dc89bf5fca020edf06bb7e25fdd0d1d2a32cb
QwertyGMR <76532709+QwertyGMR@users.noreply.github.com>
2021-01-04 19:34:20 +0100

    V3 UPDATE
    Adding damage animation, objects creation, restart button and more adaptability to touch devices.

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-/*PONG GAME v2
+/*PONG GAME v3
 by MSS
 
 ENJOY :)
@@ -8,20 +8,25 @@ ENJOY :)
 //setting up variables
 var best_score = 0;
 var score = 0;
-var paddle_x = 5;
-var paddle_y = 400;
+var paddle = {
+  x: 5,
+  y: 400
+}
 var lifes = 3;
-var scoreable = true;
-var x = 0;
-var y = 0;
-var r = 255;
-var g = 255;
-var b = 255;
+var ball = {
+  scoreable: true,
+  x: 0,
+  y: 0,
+  r: 219,
+  g: 255,
+  b: 41
+}
 
 //Difficulty is the speed at wich the ball travels
 var difficulty = 5;
 var leftright = difficulty;
 var updown = difficulty;
+var damage_animation = 0
 
 //creates canvas
 function setup() {
@@ -42,92 +47,126 @@ function draw() {
   fill(255);
   text("SCORE " + score, 500, 25);
 
-  //puts a "game over" comment when lifes get to 0
+  //initializes game over menu
   if (lifes <= 0) {
 
+    //restart button
     if (mouseIsPressed) {
-
-      restart()
+      
+      if (mouseX > 224 && mouseX < 375 && mouseY > 250 && mouseY < 310) {
+        restart();
+      }
+      
     }
 
+    //game over menu
     fill(243, 38, 22);
     textSize(100);
-    text("Game over!", 40, 200);
+    text("Game over!", 40, 120);
     fill(255);
     textSize(20);
-    text("YOUR SCORE: " + score, 220, 270);
+    text("YOUR SCORE: " + score, 220, 180);
     if (best_score < score) {
       best_score = score
     }
+    //score and best score
     fill(255);
     textSize(20);
-    text("BEST SCORE: " + best_score, 222, 300);
-    fill(234, 245, 29)
-    text("Game by MSS", 235, 350)
-    textSize(15);
-    fill(145, 145, 129)
-    text("(Press the mouse button to restart)", 190, 380)
-
+    text("BEST SCORE: " + best_score, 222, 210);
+    fill(234, 245, 29);
+    text("Game by MSS", 235, 380);
+    damage_animation = 0;
+    //restart button (text)
+    fill(255);
+    rectMode(CORNER);
+    rect(222, 250, 150, 60);
+    fill(0);
+    textSize(30);
+    text("RESTART", 229, 290);
 
   }
-  //conditional that makes that the program can only work i've the lifes are greather tan 0
+  //conditional that makes that the program can only work if the lifes are greather tan 0
   if (lifes > 0) {
 
     //this creates the paddle on the down part, and sets its cordinates/position
-    var paddle_x = mouseX;
+    paddle.x = mouseX;
     rectMode(CENTER);
     fill(243, 85, 22);
     stroke(0, 5);
     rect(mouseX, 400, 100, 20);
 
     //makes the ball turn out when it hits on the LEFT SIDE
-    if (x > width) {
+    if (ball.x > width) {
       leftright = leftright * -1;
       scoreable = true;
 
     }
     //makes the ball turn out when it hits on the RIGHT SIDE
-    if (x < 0) {
+    if (ball.x < 0) {
       leftright = leftright * -1;
       scoreable = true;
 
     }
     //makes the ball turn out when it hits on the UPPER SIDE
-    if (y < 0) {
+    if (ball.y < 0) {
       updown = updown * -1;
-      scoreable = true;
+      ball.scoreable = true;
     }
     //makes the ball turn out when it hits on the DOWN SIDE
-    if (y > 385) {
+    if (ball.y > 385) {
       updown = updown * -1;
       lifes = lifes - 1;
-      scoreable = false;
+      ball.scoreable = false;
+      damage_animation = 20;
     }
     //commands that set up when the ball has touched the paddle
-    if (x < paddle_x + 60 && x > paddle_x - 60 && y > paddle_y - 40) {
+    if (ball.x < paddle.x + 60 && ball.x > paddle.x - 60 && ball.y > paddle.y - 40) {
       updown = updown * -1;
-      if (scoreable == true) {
+      if (ball.scoreable == true) {
         score = score + 1
       }
-      scoreable = false
+      ball.scoreable = false
 
     }
     //makes the direction turn into the ball position
-    x = x + leftright;
-    y = y + updown;
+    ball.x = ball.x + leftright;
+    ball.y = ball.y + updown;
 
     //creates the ball on the x, y position
     noStroke();
-    fill(219, 225, 41);
-    ellipse(x, y, 50, 50);
+    fill(ball.r, ball.g, ball.b);
+    ellipse(ball.x, ball.y, 50, 50);
+
+    //damage animation
+    if (damage_animation <= 0) {
+
+      ball.r = 219
+      ball.g = 225
+      ball.b = 41
+
+    } else {
+
+      ball.r = 225
+      ball.g = 0
+      ball.b = 0
+
+      damage_animation = damage_animation - 1
+
+    }
+
   }
 }
 
-
+//function that resets all variables when restarting
 function restart() {
   lifes = 3;
   score = 0;
-  x = 200
-  y = 0
+  ball.x = 200
+  ball.y = 0
 
+  ball.r = 219
+  ball.g = 225
+  ball.b = 41
+
+  damage_animaiton = 0
 }
